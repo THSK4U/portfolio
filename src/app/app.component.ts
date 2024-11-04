6f972ac579d996e42a99ccea0c559a39717512a5
THSK4U <Sellaktaha1@gmail.com>
2024-11-04 02:35:40 +0100

    Start App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroComponent} from './hero/hero.component';
+import {IntroductionComponent} from './introduction/introduction.component';
+import {SkillsComponent} from './skills/skills.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {ExperienceComponent} from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, IntroductionComponent, SkillsComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
