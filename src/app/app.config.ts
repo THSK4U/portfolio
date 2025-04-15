16bf58af87822181966c5e34baa6330bda260e38
THSK4U <Sellaktaha1@gmail.com>
2025-04-15 17:00:17 +0100

    Add vercel analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 export const appConfig: ApplicationConfig = {
   providers: [
